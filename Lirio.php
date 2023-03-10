1976b09499d10085fc1a589d10b5ed577dd472ca
Aluno <ferreirabrandaomariaeduarda@gmail.com>
2023-03-10 09:40:42 -0300

    página lírio

---
+++ Lirio.php
@@ -0,0 +1,7 @@
+<?php 
+
+class Lirio extends Flores{
+
+    public function __construct($nome, $especie, $aroma, $tamanho, $ambiente) {
+        parent::__construct($nome, $especie, $aroma, $tamanho, $ambiente);
+    }

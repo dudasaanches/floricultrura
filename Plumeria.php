a5640c908ac02f9aae69913e54956c2385e2fe2d
leticia chata <leticia.munizfontanetti20@gmail.com>
2023-03-10 09:46:05 -0300

    plumeria

---
+++ Plumeria.php
@@ -0,0 +1,8 @@
+<?php 
+
+class Plumeria extends Flores {
+
+    public function __construct($nome, $especie, $cor,  $aroma, $tamanho, $ambiente) {
+        parent::__construct($nome, $especie, $cor, $aroma, $tamanho, $ambiente);
+    }
+}
